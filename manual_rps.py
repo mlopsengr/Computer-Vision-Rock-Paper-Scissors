adbda22e44c74df0714c85809d2dd1dafb9ba6be
TobiJohn <tobioluwafemi58@gmail.com>
2022-07-18 20:24:36 +0100

    starts the get prediction method

--- manual_rps.py
+++ manual_rps.py
@@ -4,7 +4,7 @@ import random
 # this python code is created to build the model for the game rock paper scissors
 
 # for milestone 4 : you can creae user score, computer score, number of rounds/run, list of choice (rock, paper, scissors, nothing) in your init method
-# also create a get_prediction function and paste the keras model code, little tweaks are needed
+# also create a get_prediction method and paste the keras model code, little tweaks are needed
 # you can use add max under np  to mapp the values 1,2,3,4 to rock, paper, scissors, nothing
 # you need to put a countdown timer to the game, so the game ends after a number of runs or time period.
 
@@ -39,6 +39,11 @@ class computer_vision:
         return self.user_choice
 
         pass
+
+    def get_prediction(self):
+
+        pass
+
 
 
     def get_winner(self) -> str:
